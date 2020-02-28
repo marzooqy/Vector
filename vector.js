2f16cfa113e206f71a5ee76cdcd6c68b2aca58f4
marzooqy <marzooqy@users.noreply.github.com>
2020-02-28 06:53:31 -0800

    Update vector.js

--- vector.js
+++ vector.js
@@ -2,79 +2,79 @@ function Vector(x, y) {
 	this.x = x;
 	this.y = y;
 
-	this.angle = function() {
-		return Math.atan2(this.y, this.x);
-	};
+Vector.prototype.angle = function() {
+	return Math.atan2(this.y, this.x);
+};
 
-	this.clone = function() {
-		return new Vector(this.x, this.y);
-	};
+this.clone = function() {
+	return new Vector(this.x, this.y);
+};
 
-	this.copy = function(vec) {
-		this.x = vec.x;
-		this.y = vec.y;
-		return this;
-	};
+this.copy = function(vec) {
+	this.x = vec.x;
+	this.y = vec.y;
+	return this;
+};
 
-	this.cross = function(vec) {
-		return this.x * vec.y - this.y * vec.x;
-	};
+this.cross = function(vec) {
+	return this.x * vec.y - this.y * vec.x;
+};
 
-	this.distance = function(vec) {
-		return Math.sqrt(Math.pow(this.x - vec.x, 2) + Math.pow(this.y - vec.y, 2));
-	};
+this.distance = function(vec) {
+	return Math.sqrt(Math.pow(this.x - vec.x, 2) + Math.pow(this.y - vec.y, 2));
+};
 
-	this.dot = function(vec) {
-		return this.x * vec.x + this.y * vec.y;
-	};
+this.dot = function(vec) {
+	return this.x * vec.x + this.y * vec.y;
+};
 
-	this.equals = function(vec) {
-		return this.x == vec.x && this.y == vec.y;
-	};
+this.equals = function(vec) {
+	return this.x == vec.x && this.y == vec.y;
+};
 
-	this.inverse = function() {
-		this.x *= -1;
-		this.y *= -1;
-		return this;
-	};
+this.inverse = function() {
+	this.x *= -1;
+	this.y *= -1;
+	return this;
+};
 
-	this.length = function() {
-		return Math.sqrt(this.x * this.x + this.y * this.y);
-	};
+this.length = function() {
+	return Math.sqrt(this.x * this.x + this.y * this.y);
+};
 
-	this.normalize = function() {
-		var length = this.length();
+this.normalize = function() {
+	var length = this.length();
 
-		if(length == 0) {
-			this.x = 1;
-			this.y = 0;
-		} else {
-			this.x /= length;
-			this.y /= length;
-		}
+	if(length == 0) {
+		this.x = 1;
+		this.y = 0;
+	} else {
+		this.x /= length;
+		this.y /= length;
+	}
 
-		return this;
-	};
+	return this;
+};
 
-	this.rotate = function(angle) {
-		var cos = Math.cos(angle);
-		var sin = Math.sin(angle);
+this.rotate = function(angle) {
+	var cos = Math.cos(angle);
+	var sin = Math.sin(angle);
 
-		var nx = this.x * cos - this.y * sin;
-		var ny = x * sin + this.y * cos;
+	var nx = this.x * cos - this.y * sin;
+	var ny = x * sin + this.y * cos;
 
-		this.x = nx;
-		this.y = ny;
+	this.x = nx;
+	this.y = ny;
 
-		return this;
-	};
+	return this;
+};
 
-	this.toString = function() {
-		return 'x:' + this.x + ', y:' + this.y;
-	};
+this.toString = function() {
+	return 'x:' + this.x + ', y:' + this.y;
+};
 
-	this.zero = function() {
-		this.x = this.y = 0;
-		return this;
-	};
+this.zero = function() {
+	this.x = this.y = 0;
+	return this;
+};
 }
